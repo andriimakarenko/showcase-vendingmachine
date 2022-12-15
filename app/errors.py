4efa97905be30d14d495c54d9fd611f8c743d761
Andy Mac <andriimakarenko@outlook.com>
2022-12-15 04:58:16 +0200

    More errors
    Signed-off-by: Andy Mac <andriimakarenko@outlook.com>

--- app/errors.py
+++ app/errors.py
@@ -5,9 +5,15 @@ class Errors(object):
     ACCESS_DENIED = "ACCESS_DENIED"
     INVALID_REQUEST = "INVALID_REQUEST"
     INVALID_TOKEN = "INVALID_TOKEN"
+    INVALID_USERNAME = "INVALID_USERNAME"
+    INVALID_LENGTH = "INVALID_LENGTH"
+    REQUIRED_FIELD = "REQUIRED_FIELD"
 
 
 class ErrorsForHumans(object):
     ACCESS_DENIED = "Access denied."
     INVALID_REQUEST = "Invalid request."
     INVALID_TOKEN = "Invalid token"
+    INVALID_USERNAME = "Please provide a valid username"
+    INVALID_LENGTH = "This field must be between 4 and 25 chars long" # Yes, I know I set this in 2 diff places
+    REQUIRED_FIELD = "This field is required"
