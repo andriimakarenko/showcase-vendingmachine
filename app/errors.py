3a133e2e8401b51a562970bb64c866509e07d438
Andy Mac <andriimakarenko@outlook.com>
2022-12-15 04:02:17 +0200

    Introduce some errors
    Signed-off-by: Andy Mac <andriimakarenko@outlook.com>

---
+++ app/errors.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+
+class Errors(object):
+    ACCESS_DENIED = "ACCESS_DENIED"
+    INVALID_REQUEST = "INVALID_REQUEST"
+    INVALID_TOKEN = "INVALID_TOKEN"
+
+
+class ErrorsForHumans(object):
+    ACCESS_DENIED = "Access denied."
+    INVALID_REQUEST = "Invalid request."
+    INVALID_TOKEN = "Invalid token"
