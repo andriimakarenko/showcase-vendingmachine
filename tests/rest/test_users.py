e073c5c1e33f1a69ed8fbbafa57da440552e65ba
Andy Mac <andriimakarenko@outlook.com>
2022-12-20 07:33:59 +0200

    Get user details tests
    Signed-off-by: Andy Mac <andriimakarenko@outlook.com>

--- tests/rest/test_users.py
+++ tests/rest/test_users.py
@@ -3,9 +3,9 @@ from flask import url_for
 
 from app import errors
 
-def test_registration_valid(client, seed_database):
+def test_user_registration_valid(client, seed_database):
     """
-    GIVEN a Flask application configured for testing
+    GIVEN a Flask-backed API configured for testing
     WHEN the '/user' URL is posted to (POST)
     THEN check the response is valid and the user is created and the token is returned
     """
@@ -20,15 +20,16 @@ def test_registration_valid(client, seed_database):
         follow_redirects=True
     )
     response_object = json.loads(response.data)
+    print(json.dumps(response_object, indent=2))
     assert response.status_code == 201
     assert 'user' in response_object
     assert 'token' in response_object and response_object['token'] is not None
     assert response_object['user']['username'] == req_json['username']
     assert response_object['user']['password'] != req_json['password']
 
-def test_registration_username_taken(client, seed_database):
+def test_user_registration_username_taken(client, seed_database):
     """
-    GIVEN a Flask application configured for testing
+    GIVEN a Flask-backed API configured for testing
     WHEN the '/user' URL is posted to (POST) with an already taken username
     THEN the response contains an error
     """
@@ -46,3 +47,32 @@ def test_registration_username_taken(client, seed_database):
     print(json.dumps(response_object, indent=2))
     assert response.status_code == 400
     assert errors.Errors.USERNAME_TAKEN in response_object['form_errors']['username']
+
+def test_user_get_details(client, seed_database):
+    """
+    GIVEN a Flask-backed API configured for testing
+    WHEN the '/user' URL is gets a GET request for an existing user from an authenticated user
+    THEN the user details are returned
+    """
+    response = client.get(
+        url_for('api.user_get_user_details', user_id=1),
+        headers={'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoQGV4YW1wbGUuY29tIiwic3ViIjoiYXV0aEBleGFtcGxlLmNvbSIsImF1ZCI6Imh0dHBzOi8vZXhhbXBsZS5jb20iLCJpYXQiOjE2NzE1MTM5NDIsImV4cCI6MTcxNDcxMzk0MiwidXNlcl9pZCI6MX0.vHOp8RyDpm3Jrc-IsK1MY6lLU_SAe9yA_LQuepjup2w'},
+        follow_redirects=True
+    )
+    response_object = json.loads(response.data)
+    assert 'user' in response_object
+    assert response_object['user']['username'] == 'mrvendor'
+
+def test_user_get_details_unauthorized(client, seed_database):
+    """
+    GIVEN a Flask-backed API configured for testing
+    WHEN the '/user' URL is gets a GET request for an existing user from an authenticated user
+    THEN the user details are returned
+    """
+    response = client.get(
+        url_for('api.user_get_user_details', user_id=1),
+        follow_redirects=True
+    )
+    response_object = json.loads(response.data)
+    assert response.status_code == 403
+    assert errors.Errors.MISSING_TOKEN in response_object['errors']
